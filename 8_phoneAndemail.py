693d3f12b37bcfa63b8cb6fdd4371a3fbb54b9e0
Barret Ren <barret.ren@outlook.com>
2018-02-07 22:09:23 +0800

    正则表达式 commit

--- 8_phoneAndemail.py
+++ 8_phoneAndemail.py
@@ -11,5 +11,28 @@ phoneRegex = re.compile(r'''(
 (\s*(ext|x|ext.)\s*(\d{2,5}))?  #extension
 )''', re.VERBOSE)
 
+emailRegex = re.compile(r'''(
+[a-zA-Z0-9._%+-]+               #username
+@
+[a-zA-Z0-9.-]+                  #domain name
+(\.[a-zA-Z]{2,4})               #dot something
+)''', re.VERBOSE)
 
+#find matches in clipboard text
+text = str(pyperclip.paste())
+matches = []
+for group in phoneRegex.findall(text):
+    phoneNum = '-'.join([group[1], group[3], group[5]])
+    if group[8] != '':
+        phoneNum += ' x' + group[8]
+    matches.append(phoneNum)
 
+for group in emailRegex.findall(text):
+    matches.append(group[0])
+
+#copy result to clipboard
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard: {0}'.format('\n'.join(matches)))
+else:
+    print('no phone number or email address')
